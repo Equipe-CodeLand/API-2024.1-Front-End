032f2d812884ee0a15563f7e13c65ac269c7c358
Luiz Felipe <felipe.pinghang2@gmail.com>
2024-03-26 20:31:49 -0300

    adicionando menu hamburguer

--- src/components/navbar.component.tsx
+++ src/components/navbar.component.tsx
@@ -1,6 +1,17 @@
 import styles from "../styles/navbar.module.css"
+import { useState } from "react";
 
 export default function Navbar() {
+    const [active, setMode] = useState(false);
+    const toggleMode = () => {
+        setMode(!active);
+        if (active) {
+            console.log("Fechado")
+        } else {
+            console.log("Aberto")
+        }
+    }
+
     return (
         <nav className={styles.navbar}>
             <div className={styles.logo}>
@@ -11,6 +22,16 @@ export default function Navbar() {
                     <li><a href="/ativos">Ativos</a></li>
                     <li><a href="/manutencao">Manutenções</a></li>
                 </ul>
+            </div>
+            <div className={!active ? styles.open : styles.closed }>
+                <div className={styles.background} onClick={toggleMode}></div>
+                <div className={styles.hamburguer} onClick={toggleMode}></div>
+                <div className={styles.menu}>
+                    <ul>
+                        <li><a href="/ativos">Ativos</a></li>
+                        <li><a href="/manutencao">Manutenções</a></li>
+                    </ul>
+                </div>
             </div>
         </nav>
     );
