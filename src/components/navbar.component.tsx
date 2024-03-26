eb1f11299da9cdcafc15adc230dbee133a940a55
Luiz Felipe <felipe.pinghang2@gmail.com>
2024-03-26 18:19:47 -0300

    adicionando logo e nav

--- src/components/navbar.component.tsx
+++ src/components/navbar.component.tsx
@@ -3,12 +3,15 @@ import styles from "../styles/navbar.module.css"
 export default function Navbar() {
     return (
         <nav className={styles.navbar}>
-            <ul>
-                <li><a href='/'>Home</a></li>
-                <li><a href='/ativos'>Ativos</a></li>
-                <li><a href='/manutencao'>Manutenção</a></li>
-            </ul>
-            <hr />
+            <div className={styles.logo}>
+                <a href="/">Nome do Sistema</a>
+            </div>
+            <div className={styles.nav}>
+                <ul>
+                    <li><a href="/ativos">Ativos</a></li>
+                    <li><a href="/manutencao">Manutenções</a></li>
+                </ul>
+            </div>
         </nav>
     );
 }
