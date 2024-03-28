2076ae589ac940d4c053926fc956dab05e132cc7
Luiz Felipe <felipe.pinghang2@gmail.com>
2024-03-28 20:16:09 -0300

    adicionando a pagina ativo e sua base

--- src/components/ativosPage.component.tsx
+++ src/components/ativosPage.component.tsx
@@ -1,8 +1,17 @@
+import styles from "../styles/ativosPage.module.css"
+
 export default function AtivosPage() {
     return (
-        <div>
-            <h1>Ativos</h1>
-            <p>Esta é a página de ativos</p>
+        <div className={styles.body}>
+            <div className={styles.filtro}></div>
+            <div className={styles.conteudo}>
+                <main>
+                    <div className={styles.adicionarAtivo}></div>
+                    <div className={styles.listarAtivo}>
+                        
+                    </div>
+                </main>
+            </div>
         </div>
     );
 }
