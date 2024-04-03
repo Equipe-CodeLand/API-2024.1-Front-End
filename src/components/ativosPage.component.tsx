d82f3e503a0be0bf097c683a0933c69e3e302a15
Luiz Felipe <felipe.pinghang2@gmail.com>
2024-04-02 21:15:17 -0300

    finalizando o style para versao desktop da pagina ativos

--- src/components/ativosPage.component.tsx
+++ src/components/ativosPage.component.tsx
@@ -4,16 +4,24 @@ import Ativo from "./ativo.component";
 const ativos = [
     {id: 1, nome: "Maquina de Impressão 3D Industrial", disponibilidade: "1"},
     {id: 2, nome: "Maquina de Impressão 3D Industrial", disponibilidade: "2"},
-    {id: 3, nome: "Maquina de Impressão 3D Industrial", disponibilidade: "3"},
+    {id: 3, nome: "Maquina de Impressão 3D Industrial", disponibilidade: "3"}
 ]
 
 export default function AtivosPage() {
+    function adicionarAtivo() {
+        console.log("Adicionar Ativo")
+    }
+
     return (
         <div className={styles.body}>
             <div className={styles.filtro}></div>
             <div className={styles.conteudo}>
                 <main>
-                    <div className={styles.adicionarAtivo}></div>
+                    <div className={styles.adicionarAtivo}>
+                        <button className={styles.botao} onClick={adicionarAtivo}>
+                            Adicionar Ativo
+                        </button>
+                    </div>
                     <div className={styles.listarAtivo}>
                         {
                             ativos.map(ativo => {
