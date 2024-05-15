9f82cbc24686930b1c1e1208b3ebaf0f1a8873ba
leticiacarvalho04 <leticiahelena.oliveira@outlook.com>
2024-05-15 08:33:52 -0300

    feat: Criando o filtro das manutenções

--- src/pages/manutencaoPage.tsx
+++ src/pages/manutencaoPage.tsx
@@ -1,100 +1,194 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import Footer from "../components/footer";
 import ManutencaoComponent from "../components/manutencao";
 import Navbar from "../components/navbar";
 import { Manutencao } from "../types/manutencao.type";
 import styles from "../styles/manutencaoPage.module.css";
 import { useAxios } from "../hooks/useAxios";
-import { Link } from "react-router-dom";
 
 export default function ManutencaoPage() {
-
-  let [manutencoes, setManutencoes] = useState<Manutencao[]>([])
-  const [error, setError] = useState<Error | unknown>(null)
-  const { get, loading, setLoading } = useAxios()
+  const [manutencoes, setManutencoes] = useState<Manutencao[]>([]);
+  const [error, setError] = useState<Error | unknown>(null);
+  const { get, loading, setLoading } = useAxios();
+  const [idInput, setIdInput] = useState("");
+  const [nomeInput, setNomeInput] = useState("");
+  const [dataInicio, setDataInicio] = useState("");
+  const [dataFinal, setDataFinal] = useState("");
+  const [filtro, setFiltro] = useState("");
 
   useEffect(() => {
-    buscarManutencoes()
-  }, [])
+    buscarManutencoes();
+  }, []);
 
   const buscarManutencoes = async () => {
-    get("/manutencao")
-      .then(response => {
-        let manutencoes = response.data.map((manutencao: any) => {
-          return {
-            id: manutencao.id,
-            nome: manutencao.ativos.nome,
-            dataInicio: manutencao.data_inicio,
-            dataFinal: manutencao.data_final,
-            ativos_id: manutencao.ativos.id,
-            localizacao: manutencao.localizacao,
-            responsavel: manutencao.responsavel
-          }
-        })
-        setManutencoes(manutencoes)
-        setLoading(false)
-      })
-      .catch(err => {
-        setError(err)
-        setLoading(false)
-      })
-      
+    try {
+      const response = await get("/manutencao");
+      const manutencoesData = response.data.map((manutencao: any) => ({
+        id: manutencao.id,
+        nome: manutencao.ativos.nome,
+        dataInicio: manutencao.data_inicio,
+        dataFinal: manutencao.data_final,
+        ativos_id: manutencao.ativos.id,
+        localizacao: manutencao.localizacao,
+        responsavel: manutencao.responsavel
+      }));
+      setManutencoes(manutencoesData);
+      setLoading(false);
+    } catch (err) {
+      setError(err);
+      setLoading(false);
+    }
+  };
+
+  const limpar = () => {
+    limparFiltros();
+    window.location.reload();
   }
 
-  var render
-  if (loading) {
-    render =
-      <div className={styles.listarManutencao}>
-        <span className={styles.semManutencao}>
-          Carregando manutenções...
-        </span>
-      </div>
-  } else if (error) {
-    render =
-      <div className={styles.listarManutencao}>
-        <span className={styles.semManutencao}>
-          Erro ao carregar manutenções! <br />
-          :(
-        </span>
-      </div>
-  } else if (manutencoes.length > 0) {
-    render =
-      <div className={styles.listarManutencao}>
-        {manutencoes.map(manutencao => {
-          return <ManutencaoComponent
-            id={manutencao.id}
-            nome={manutencao.nome}
-            dataInicio={manutencao.dataInicio}
-            dataFinal={manutencao.dataFinal}
-            localizacao={manutencao.localizacao}
-            responsavel={manutencao.responsavel}
-            key={manutencao.id}
-            ativos_id={manutencao.ativos_id}
-            buscarManutencao={buscarManutencoes}
-          ></ManutencaoComponent>
-        })}
-      </div>
-  } else {
-    render =
-      <div className={styles.listarManutencao}>
-        <span className={styles.semManutencao}>
-          Sem manutenções cadastradas
-        </span>
-      </div>
-  }
+  const limparFiltros = () => {
+    setIdInput("");
+    setNomeInput("");
+    setDataInicio("");
+    setDataFinal("");
+    setFiltro("");
+  };
+
+  const filtrar = async () => {
+    setLoading(true);
+
+    try {
+      let response;
+
+      if (idInput) {
+        response = await get(`/manutencao/${idInput}`);
+      } else if (nomeInput) {
+        response = await get(`/manutencao/filtrar/${nomeInput}`);
+      } else if (dataInicio) {
+        const dataInicioFormatada = formatarData(dataInicio);
+        response = await get(`/manutencao/filtrar/dataInicio?dataInicio=${dataInicioFormatada}`);
+      } else if (dataFinal) {
+        const dataFinalFormatada = formatarData(dataFinal);
+        response = await get(`/manutencao/filtrar/dataFinal?dataFinal=${dataFinalFormatada}`);
+      } else {
+        return;
+      }
+
+      let data = Array.isArray(response.data) ? response.data : [response.data];
+
+      const manutencoesFiltradas = data.map((manutencao: any) => ({
+        id: manutencao.id,
+        nome: manutencao.ativos.nome,
+        dataInicio: manutencao.data_inicio ? formatarData(manutencao.data_inicio.join('-')) : "",
+        dataFinal: manutencao.data_final ? formatarData(manutencao.data_final.join('-')) : "",
+        ativos_id: manutencao.ativos.id,
+        localizacao: manutencao.localizacao,
+        responsavel: manutencao.responsavel
+      }));   
+
+      console.log(manutencoesFiltradas)
+      setManutencoes(manutencoesFiltradas);
+      setLoading(false);
+      limparFiltros(); 
+    } catch (error) {
+      console.error("Erro ao filtrar manutenções:", error);
+      setError(error);
+      setLoading(false);
+    }
+  };  
+
+  const formatarData = (data: string) => {
+    const [ano, mes, dia] = data.split("-");
+    return `${ano}-${mes}-${dia}`;
+  };
 
   return (
     <div>
       <Navbar local="manutencao" />
       <div className={styles.body}>
-        <div className={styles.filtro}></div>
+        <div className={styles.filtro}>
+          <h2>Filtro</h2>
+          <div className={styles.inputs}>
+            <select
+              name=""
+              id="filtrarPor"
+              className={`${styles.filtrarPor}`}
+              onChange={(e) => setFiltro(e.target.value)}
+            >
+              <option value="">Filtrar por</option>
+              <option value="ID">ID</option>
+              <option value="Nome">Nome</option>
+            </select>
+            {filtro === "ID" && (
+              <input
+                type="number"
+                placeholder="ID"
+                value={idInput}
+                onChange={(e) => setIdInput(e.target.value)}
+              />
+            )}
+            {filtro === "Nome" && (
+              <input
+                type="text"
+                placeholder="Nome"
+                value={nomeInput}
+                onChange={(e) => setNomeInput(e.target.value)}
+              />
+            )}
+            <hr />
+            <div>
+              <label>Data Início</label>
+              <input
+                type="date"
+                placeholder="Data Início"
+                value={dataInicio}
+                onChange={(e) => setDataInicio(e.target.value)}
+              />
+            </div>
+            <div>
+              <label>Data Final</label>
+              <input
+                type="date"
+                placeholder="Data Final"
+                value={dataFinal}
+                onChange={(e) => setDataFinal(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className={styles.filtrar}>
+            <button onClick={limpar}>Limpar</button>
+            <button onClick={filtrar}>Aplicar</button>
+          </div>
+        </div>
         <div className={styles.conteudo}>
           <main>
             <div className={styles.adicionarManutencao}>
-              <Link className={styles.botao} to="/cadastrar/manutencoes">Adicionar Manutenções</Link>
+              <Link className={styles.botao} to="/cadastrar/manutencoes">
+                Adicionar Manutenções
+              </Link>
             </div>
             <div className={styles.listarManutencao}>
-              {render}
+              {loading ? (
+                <span className={styles.semManutencao}>Carregando manutenções...</span>
+              ) : error ? (
+                <span className={styles.semManutencao}>Erro ao carregar manutenções! :(</span>
+              ) : manutencoes.length > 0 ? (
+                manutencoes.map((manutencao) => (
+                  <ManutencaoComponent
+                    id={manutencao.id}
+                    nome={manutencao.nome}
+                    dataInicio={manutencao.dataInicio}
+                    dataFinal={manutencao.dataFinal}
+                    localizacao={manutencao.localizacao}
+                    responsavel={manutencao.responsavel}
+                    key={manutencao.id}
+                    ativos_id={manutencao.ativos_id}
+                    buscarManutencao={filtro ? undefined : buscarManutencoes}
+                  />
+                ))
+              ) : (
+                <span className={styles.semManutencao}>Sem manutenções cadastradas</span>
+              )}
             </div>
           </main>
         </div>
