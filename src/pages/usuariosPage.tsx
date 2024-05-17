898f68871e8bb1fd396d85d065e3099b407cd4e4
Luiz Felipe <felipe.pinghang2@gmail.com>
2024-05-17 10:26:04 -0300

    feat: adicionando funcionabilidade do filtro de usuarios

--- src/pages/usuariosPage.tsx
+++ src/pages/usuariosPage.tsx
@@ -9,9 +9,13 @@ import { Link } from "react-router-dom"
 
 export default function UsuariosPage() {
     const [data, setData] = useState<Array<any>>([])
+    const [usuariosFiltrados, setUsuariosFiltrados] = useState<Array<any>>([])
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState<Error | unknown>(null)
     const [ativos, setAtivos] = useState<Array<any>>([])
+    const [id, setId] = useState("")
+    const [nome, setNome] = useState("")
+    const [tipoUsuario, setTipoUsuario] = useState("")
     const { get } = useAxios()
 
     const chamarAtivos = async () => {
@@ -25,12 +29,38 @@ export default function UsuariosPage() {
         get("/usuario/listar")
             .then(response => {
                 setData(response.data)
+                setUsuariosFiltrados(response.data)
                 setLoading(false)
             })
             .catch(error => {
                 setError(error)
                 setLoading(false)
             })
+    }
+
+    const filtrarUsuarios = () => {
+        let usuarios = data
+
+        if (id != "") {
+            usuarios = usuarios.filter((usuario) => usuario.id == id)
+        }
+
+        if (nome != "") {
+            usuarios = usuarios.filter((usuario) => usuario.nome == nome)
+        }
+
+        if (tipoUsuario != "") {
+            usuarios = usuarios.filter((usuario) => usuario.cargo.id == tipoUsuario)
+        }
+
+        setUsuariosFiltrados(usuarios)
+        limparFiltros()
+    }
+
+    const limparFiltros = () => {
+        setId("")
+        setNome("")
+        setTipoUsuario("")
     }
 
     useEffect(() => {
@@ -55,26 +85,26 @@ export default function UsuariosPage() {
                     :(
                 </div>
             </div>
-    } else if (data.length > 0) {
-        render = 
-        <div className={styles.listarUsuario}>
-            {data.map((usuario) => {
-                if (usuario !== undefined) {
-                    return (
-                        <Usuario
-                            id = {usuario.id}
-                            nome = {usuario.nome}
-                            cargo = {usuario.cargo.nome}
-                            cpf = {usuario.credencial.cpf}
-                            key = {usuario.id}
-                            buscarUsuarios={usuarios}
-                            estaAtivo={usuario.estaAtivo}
-                            ativos = {ativos.filter((ativo: any) => ativo.usuario?.id === usuario?.id)}
-                         />
-                    )
-                }
-            })}
-        </div>
+    } else if (usuariosFiltrados.length > 0) {
+        render =
+            <div className={styles.listarUsuario}>
+                {usuariosFiltrados.map((usuario) => {
+                    if (usuario !== undefined) {
+                        return (
+                            <Usuario
+                                id={usuario.id}
+                                nome={usuario.nome}
+                                cargo={usuario.cargo.nome}
+                                cpf={usuario.credencial.cpf}
+                                key={usuario.id}
+                                buscarUsuarios={usuarios}
+                                estaAtivo={usuario.estaAtivo}
+                                ativos={ativos.filter((ativo: any) => ativo.usuario?.id === usuario?.id)}
+                            />
+                        )
+                    }
+                })}
+            </div>
     } else {
         render =
             <div className={styles.listarUsuario}>
@@ -92,24 +122,38 @@ export default function UsuariosPage() {
                 <div className={styles.filtro}>
                     <h2>Filtro</h2>
                     <div className={styles.inputs}>
-                        <input type="text" placeholder="ID" />
-                        <input type="text" placeholder="Nome" />
+                        <input
+                            type="text"
+                            placeholder="ID"
+                            onChange={(e) => setId(e.target.value)}
+                            value={id}
+                        />
+                        <input
+                            type="text"
+                            placeholder="Nome"
+                            onChange={(e) => setNome(e.target.value)}
+                            value={nome}
+                        />
                         <hr />
-                        <select name="" id="tipoUsuario">
-                            <option value=""></option>
-                            <option value="">Administrador</option>
+                        <select
+                            onChange={(e) => setTipoUsuario(e.target.value)}
+                            value={tipoUsuario}
+                        >
+                            <option value="">Filtrar por</option>
+                            <option value="1">Administrador</option>
+                            <option value="2">Funcionário</option>
                         </select>
                     </div>
                     <div className={styles.filtrar}>
-                        <button>Aplicar</button>
+                        <button onClick={filtrarUsuarios}>Aplicar</button>
                     </div>
                 </div>
                 <div className={styles.conteudo}>
                     <main>
                         <div className={styles.botoes}>
-                            <div className={styles.trocarSenha}>
-                                <Link className={styles.botao} to="/alteracao/senha">
-                                    Trocar minha senha
+                            <div className={styles.botaoFiltro}>
+                                <Link className={styles.botao} to="#">
+                                    Filtros
                                 </Link>
                             </div>
                             <div className={styles.adicionarUsuario}>
